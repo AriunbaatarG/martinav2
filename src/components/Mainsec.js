aa38f6416a4f4d76ac71755f6f2f1439685b10ca
AriunbaatarG <g.ariunbaatar861@gmail.com>
2024-03-06 16:05:49 +0100

    main

--- src/components/Mainsec.js
+++ src/components/Mainsec.js
@@ -5,6 +5,29 @@ import Img2 from '../assets/Event-Photography/Gemini/9.JPG';
 import Img3 from '../assets/Event-Photography/Gemini/4.JPG';
 import Img4 from '../assets/Event-Photography/Gemini/5.JPG';
 import Img5 from '../assets/Event-Photography/Gemini/6.JPG';
+import Img6 from '../assets/Event-Photography/Dancestoned2021/ds9.jpg';
+import Img7 from '../assets/Event-Photography/Dancestoned2021/ds1.JPG';
+import Img8 from '../assets/Event-Photography/Spotlight 2023/sl1.jpeg';
+import Img9 from '../assets/Event-Photography/Dancestoned2021/2.JPG';
+import Img10 from '../assets/Event-Photography/Dancestoned2021/1.JPG';
+import Img11 from '../assets/Event-Photography/Dancestoned2021/6.JPG';
+import Img12 from '../assets/Editorials/ed1.JPG';
+import Img13 from '../assets/Editorials/ed3.jpeg';
+import Img14 from '../assets/Editorials/ed8.jpg';
+import Img15 from '../assets/Landscape/Vlora/v2.JPG';
+import Img16 from '../assets/Landscape/Bremen Freimarkt/bf2.JPG';
+import Img17 from '../assets/Landscape/Schaffhausen/sh2.JPG';
+import Img18 from '../assets/Landscape/lon1.JPG';
+import Img19 from '../assets/Landscape/lon2.JPG';
+import Img20 from '../assets/Landscape/Vlora/v7.JPG';
+
+
+
+
+
+
+
+
 
 
 function MainSec() {
@@ -50,7 +73,7 @@ function MainSec() {
             src = {Img5}
             width={400}
             height={600}
-            alt='gemini6'
+            alt='gemini7'
             />
         </div>
             
--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,8 +1,8 @@
 import Header from "../components/Header";
-import Footer from "../components/Footer"
-import emailjs from '@emailjs/browser'
+import Footer from "../components/Footer";
+import emailjs from '@emailjs/browser';
 import { useRef, useState } from "react";
-import ReCAPTCHA from 'react-google-recaptcha'
+import ReCAPTCHA from 'react-google-recaptcha';
 
 function Contact() {
     const form = useRef();
@@ -10,50 +10,76 @@ function Contact() {
         username: "",
         email: "",
         message: ""
-    })
-    const sendEmail = (e) => {
-    e.preventDefault();
-
-    emailjs
-      .sendForm('service_3o2ctco', 'template_uhan5bc', form.current, {
-        publicKey: 'bRp1E_2AjdyrWol5f',
-      })
-      .then(
-        () => {
-          console.log('SUCCESS!');
-        },
-        (error) => {
-          console.log('FAILED...', error.text);
-        },
-      );
-    
-    setFormValues({
-        username: "",
-        email: "",
-        message: ""
     });
-  };
+    const [recaptchaFilled, setRecaptchaFilled] = useState(false); // State to track ReCAPTCHA filled status
+    const [submitted, setSubmitted] = useState(false); // State to track form submission
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        if (!recaptchaFilled) { // Check if ReCAPTCHA is filled
+            alert("Please fill in the ReCAPTCHA.");
+            return;
+        }
+
+        emailjs
+            .sendForm('service_3o2ctco', 'template_uhan5bc', form.current, {
+                publicKey: 'bRp1E_2AjdyrWol5f',
+            })
+            .then(
+                () => {
+                    console.log('SUCCESS!');
+                    setFormValues({ // Reset formValues state to empty values
+                        username: "",
+                        email: "",
+                        message: ""
+                    });
+                    setSubmitted(true); // Set submitted state to true after successful submission
+                },
+                (error) => {
+                    console.log('FAILED...', error.text);
+                },
+            );
+    };
+
+    const handleRecaptchaChange = (value) => {
+        if (value) {
+            setRecaptchaFilled(true);
+        }
+    };
+
     return (
         <div>
             <Header/>
             <div className="contact-form">
-                <form ref={form} onSubmit={sendEmail}>
-                    <label>Name</label>
-                    <input 
-                        type="text" 
-                        name="user_name" 
-                    />
-                    <label>Email</label>
-                    <input 
-                        type="email" 
-                        name="user_email"
-                    />
-                    <label>Message</label>
-                    <textarea name="message" />
-                    <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} />
-                    <button type="submit" value="Send">Submit</button>
-                    
-                </form>
+                {!submitted ? (
+                    <form ref={form} onSubmit={sendEmail}>
+                        <label>Name</label>
+                        <input 
+                            type="text" 
+                            name="user_name"
+                            value={formValues.username}
+                            onChange={(e) => setFormValues({...formValues, username: e.target.value})}
+                        />
+                        <label>Email</label>
+                        <input 
+                            type="email" 
+                            name="user_email"
+                            value={formValues.email}
+                            onChange={(e) => setFormValues({...formValues, email: e.target.value})}
+                        />
+                        <label>Message</label>
+                        <textarea 
+                            name="message"
+                            value={formValues.message}
+                            onChange={(e) => setFormValues({...formValues, message: e.target.value})}
+                        />
+                        <ReCAPTCHA sitekey={process.env.REACT_APP_SITE_KEY} onChange={handleRecaptchaChange} />
+                        <button type="submit" value="Send">Submit</button>
+                    </form>
+                ) : (
+                    <p>Thank you for contacting me! I will get back to you as soon as I can.</p>
+                )}
             </div>
             <Footer/>
         </div>
